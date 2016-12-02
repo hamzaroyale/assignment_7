34cb2d53cffd1780cff1c1e5b1fb3794368f8fe8
hamzaroyale <hamzaroyale@gmail.com>
2016-12-02 13:19:02 -0800

    Add files via upload

---
+++ gaddis_7th_ed_chap_9_prob_1_account_validation/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Hatim, Muhamamd
+ * Created on: determine if the number is valid by checking for it in a list
+ * Purpose:
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output objects
+//#include <iomanip>
+using namespace std; //Name-space used in the System Library
+
+//User Libraries
+//Global Constants
+//Function prototypes
+
+//Execution Begins Here:
+int main(int argc, char** argv) {
+    
+    //Declaration of Variables
+    
+    
+    //ask for values
+    
+    
+    //Process values
+    
+    
+    //Outputs answers
+    
+    
+    //exit program
+    return 0;
+}
